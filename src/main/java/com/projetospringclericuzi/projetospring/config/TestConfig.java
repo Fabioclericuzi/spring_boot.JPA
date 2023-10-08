2a616db91bb7cc7e0be389b682951e8512bcb5c4
unknown <fabioclericuzilima@hotmail.com>
2023-10-08 18:25:55 -0300

    Adição da classe Enum e alterações nas classes testconfig e order para ajustes

--- src/main/java/com/projetospringclericuzi/projetospring/config/TestConfig.java
+++ src/main/java/com/projetospringclericuzi/projetospring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetospringclericuzi.projetospring.entities.Order;
 import com.projetospringclericuzi.projetospring.entities.User;
+import com.projetospringclericuzi.projetospring.entities.enums.OrderStatus;
 import com.projetospringclericuzi.projetospring.repositories.OrderRepository;
 import com.projetospringclericuzi.projetospring.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(2, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
 		
-		Order o1 = new Order(10, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(11, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(12, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(10, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(11, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(12, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
