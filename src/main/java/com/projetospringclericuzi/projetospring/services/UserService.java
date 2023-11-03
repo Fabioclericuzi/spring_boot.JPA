a4495089b39f438e804f3b08738dbb1bc77ef890
unknown <fabioclericuzilima@hotmail.com>
2023-11-03 19:40:27 -0300

    Adição das classes de tratamento de exceções

--- src/main/java/com/projetospringclericuzi/projetospring/services/UserService.java
+++ src/main/java/com/projetospringclericuzi/projetospring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetospringclericuzi.projetospring.entities.User;
 import com.projetospringclericuzi.projetospring.repositories.UserRepository;
+import com.projetospringclericuzi.projetospring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
